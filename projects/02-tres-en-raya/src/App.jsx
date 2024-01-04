1dba52a8e1aea9d3157c5d918c263faeec4eca1b
jcarrillo <130939221+JudaCarrillo@users.noreply.github.com>
2024-01-04 14:35:35 -0500

    Extract to component Tic tac toe

---
+++ projects/02-tres-en-raya/src/App.jsx
@@ -0,0 +1,63 @@
+import confetti from "canvas-confetti";
+import { useState } from "react";
+import "./App.css";
+import { Square, WinnerModal } from "./components";
+import { TURNS } from "./constants";
+import { checkEndGame, checkWinnerFrom } from "./logic";
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null);
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return;
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+
+    const newWinner = checkWinnerFrom(newBoard);
+    if (newWinner) {
+      confetti();
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
+    }
+  };
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
+  return (
+    <main className="board">
+      <h1>Tres en raya</h1>
+      <button onClick={resetGame}>Reiniciar juego</button>
+
+      <section className="game">
+        {board.map((valueSquare, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {valueSquare}
+            </Square>
+          );
+        })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner} />
+    </main>
+  );
+}
+
+export default App;
